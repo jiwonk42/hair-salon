1b665b11f4919ca3690f89a88bb69f3b610314b7
Jiwon Kang <jiwonk42@gmail.com>
2017-02-24 10:30:43 -0800

    add tests and methods for Stylist object similar to Client object

---
+++ Objects/Stylist.cs
@@ -0,0 +1,170 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace HairSalon
+{
+  public class Stylist
+  {
+    private int _id;
+    private string _name;
+    private string _specialize;
+
+    public Stylist(string Name, string Specialize, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+      _specialize = Specialize;
+    }
+
+    public override bool Equals(System.Object otherStylist)
+    {
+        if (!(otherStylist is Stylist))
+        {
+          return false;
+        }
+        else
+        {
+          Stylist newStylist = (Stylist) otherStylist;
+          bool idEquality = this.GetId() == newStylist.GetId();
+          bool nameEquality = this.GetName() == newStylist.GetName();
+          bool specializeEquality = this.GetSpecialize() == newStylist.GetSpecialize();
+          return (idEquality && nameEquality && specializeEquality);
+        }
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetSpecialize()
+    {
+      return _specialize;
+    }
+
+    public void SetSpecialize(string newSpecialize)
+    {
+      _specialize = newSpecialize;
+    }
+
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylists = new List<Stylist>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int stylistId = rdr.GetInt32(0);
+        string stylistName = rdr.GetString(1);
+        string stylistSpecialize = rdr.GetString(2);
+        Stylist newStylist = new Stylist(stylistName, stylistSpecialize, stylistId);
+        allStylists.Add(newStylist);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allStylists;
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO stylists (name, specialization) OUTPUT INSERTED.id VALUES (@StylistName, @StylistSpecialize);", conn);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@StylistName";
+      nameParameter.Value = this.GetName();
+
+      SqlParameter specializeParameter = new SqlParameter();
+      specializeParameter.ParameterName = "@StylistSpecialize";
+      specializeParameter.Value = this.GetSpecialize();
+
+      cmd.Parameters.Add(nameParameter);
+      cmd.Parameters.Add(specializeParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static Stylist Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists WHERE id = @StylistId;", conn);
+      SqlParameter stylistIdParameter = new SqlParameter();
+      stylistIdParameter.ParameterName = "@StylistId";
+      stylistIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(stylistIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      int foundStylistId = 0;
+      string foundStylistName = null;
+      string foundStylistSpecialize = null;
+
+      while(rdr.Read())
+      {
+        foundStylistId = rdr.GetInt32(0);
+        foundStylistName = rdr.GetString(1);
+        foundStylistSpecialize = rdr.GetString(2);
+      }
+      Stylist foundStylist = new Stylist(foundStylistName, foundStylistSpecialize, foundStylistId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundStylist;
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM stylists;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+  }
+}
