de0134b7f6a1cf386c9ec872748971aaf01d9f06
Jiwon Kang <jiwonk42@gmail.com>
2017-02-24 13:08:08 -0800

    add and create success.cshtml, client.cshtml, and cleared.cshtml

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -22,6 +22,55 @@ namespace HairSalon
             };
 
 
+            Get["/clients"] = _ => {
+              List<Client> AllClients = Client.GetAll();
+              return View["clients.cshtml", AllClients];
+            };
+            Get["/stylists"] = _ => {
+              List<Stylist> AllStylists = Stylist.GetAll();
+              return View["stylists.cshtml", AllStylists];
+            };
+
+
+            Get["/stylists/new"] = _ => {
+              return View["stylists_form.cshtml"];
+            };
+
+
+            // success.cshtml -> stylists.cshtml
+            Post["/stylists/new"] = _ => {
+              Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["stylist-specialize"]);
+              newStylist.Save();
+              return View["success.cshtml"];
+            };
+
+
+            Get["/clients/new"] = _ => {
+              List<Stylist> AllStylists = Stylist.GetAll();
+              return View["clients_form.cshtml", AllStylists];
+            };
+
+
+            // success.cshtml -> clients.cshtml
+            Post["/clients/new"] = _ => {
+              Client newClient = new Client(Request.Form["client-name"], Request.Form["client-phone"], Request.Form["client-address"], Request.Form["stylist-id"]);
+              newClient.Save();
+              return View["success.cshtml"];
+            };
+
+            Post["/clients/delete"] = _ => {
+              Client.DeleteAll();
+              return View["cleared.cshtml"];
+            };
+
+            Get["/stylists/{id}"] = parameters => {
+              Dictionary<string, object> model = new Dictionary<string, object>();
+              var SelectedStylist = Stylist.Find(parameters.id);
+              var StylistClients = SelectedStylist.GetClients();
+              model.Add("stylist", SelectedStylist);
+              model.Add("clients", StylistClients);
+              return View["stylist.cshtml", model];
+            };
         }
     }
 }
