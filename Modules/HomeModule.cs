fabb2d01141ffe18f27e49e3b1895248a0de912a
Jiwon Kang <jiwonk42@gmail.com>
2017-02-24 12:17:59 -0800

    create and add tasks.cshtml

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,6 +12,14 @@ namespace HairSalon
         List<Stylist> AllStylists = Stylist.GetAll();
         return View["index.cshtml", AllStylists];
       };
+      Get["/clients"] = _ => {
+        List<Client> AllClients = Client.GetAll();
+        return View["clients.cshtml", AllClients];
+      };
+      Get["/stylists"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["stylists.cshtml", AllStylists];
+      };
     }
   }
 }
